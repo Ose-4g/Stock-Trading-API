ad18796e60a39167901bc37961b5b57dc08586fc
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-10-31 20:10:01 +0100

    feat: update the routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth';
 import userRouter from './user';
+import financeRouter from './finance';
 
 const router: Router = Router();
 
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
+router.use('/finance', financeRouter);
 
 export default router;
