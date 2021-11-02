b732647566c14d8c15e5216cd7f8664693775e83
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2021-11-02 13:28:17 +0100

    feat: add cors to the code

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ import cookieParser from 'cookie-parser';
 import errorMiddleWare from './errors/errorHandler';
 import morgan from 'morgan';
 import router from './routes/index';
+import cors from 'cors';
 
 // import router
 
 //add middlewares
 const app = express();
 
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
